7a877dce56d244ab3ba8a138e3483e6c9e502d22
Marlon Secundo <marlon_secundo@outlook.com>
2022-01-10 23:49:40 -0300

    feat: alert dialog

--- app/src/main/java/com/example/weather/FavoritesActivity.java
+++ app/src/main/java/com/example/weather/FavoritesActivity.java
@@ -2,12 +2,20 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FavoritesActivity extends AppCompatActivity {
 
@@ -18,15 +26,6 @@ public class FavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
-
-        favoritesListView = (ListView) findViewById(R.id.favListView);
-
-        TextView v = (TextView) findViewById(R.id.textView12);
-
-//        favoritesListView.addView(v);
-
-
-
 
 
 
--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
         favButton = findViewById(R.id.favButton);
 
+        infoButton = findViewById(R.id.infoButton);
+
+
+
+
+
         favButton.setOnClickListener(favButtonListener);
+
+        infoButton.setOnClickListener(infoButtonListener);
+
     }
 
     View.OnClickListener favButtonListener = new View.OnClickListener() {
@@ -34,6 +46,46 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+
+    View.OnClickListener infoButtonListener = new View.OnClickListener() {
+
+        public void onClick(View v) {
+
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    if (!isFinishing()){
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Info")
+                                .setMessage("Alunos: Marlon, Pedro Hugo, Antony")
+                                .setCancelable(false)
+                                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Uri uri = Uri.parse("https://github.com/marlonsecundo/weather-app/"); // missing 'http://' will cause crashed
+                                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                                        startActivity(intent);
+
+                                    }
+                                }).setNegativeButton(
+                                "Sair",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                }).show();
+                    }
+                }
+            });
+
+
+
+        }
+    };
+
 
 
 
